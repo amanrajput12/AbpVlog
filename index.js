7fcb3e2e6dd9a4599941954abafe26e14c6591af
Aman Rajput <rajputaman724857@gmail.com>
2024-08-29 17:33:47 +0530

    cors origin change

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express()
 dotev.config()
 connectDb()
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://apbvlogs.netlify.app',
 
 }))
 app.use(express.json())
