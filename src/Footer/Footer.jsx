3915838311dde1a9abc91b8c6f35222889c12ddd
Aman Rajput <rajputaman724857@gmail.com>
2024-09-02 22:49:48 +0530

    footer change

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -3,9 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className=" w-full py-6 text-center text-white text-sm shadow-inner">
-      <div>
-        Locations: 2nd Floor, 113, 2nd Cross Rd, East of NGEF Layout, Kasturi Nagar, Bangalore, Karnataka 560043
-      </div>
+      
       <div className="mt-2">
         <a className="text-blue-400 hover:underline">Terms & Conditions</a> | 
         <a  className="text-blue-400 hover:underline ml-2">Privacy Policy</a>
