3a71eba9d26b6f2007038084eb55452cff43caab
Aman Rajput <rajputaman724857@gmail.com>
2024-09-07 00:52:30 +0530

    proxy error solve

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // http://localhost:4000
-  server:{
-    proxy:{
-      '/v1':{
-        target:"https://abpvlog.onrender.com"
+  server: {
+    proxy: {
+      '/v1': {
+        target: 'https://abpvlog.onrender.com',
+        changeOrigin: true,  // This will change the origin of the host header
+         
       }
     }
   }
