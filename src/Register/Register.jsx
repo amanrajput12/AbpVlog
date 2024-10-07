5ae7f663a8f79323bb277098c0ef6a6e38abbb96
Aman Rajput <rajputaman724857@gmail.com>
2024-10-07 23:03:39 +0530

    payment photo add

--- src/Register/Register.jsx
+++ src/Register/Register.jsx
@@ -1,68 +1,73 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import toast, { Toaster } from 'react-hot-toast';
-import { useNavigate } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import { RegisterSucess } from '../Singup/RegisterSlice.js';
-import ReactLoading from 'react-loading';
+import React, { useState } from "react";
+import axios from "axios";
+import toast, { Toaster } from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { RegisterSucess } from "../Singup/RegisterSlice.js";
+import ReactLoading from "react-loading";
+import Payment from "../../public/Payment.png";
 
 const Register = () => {
-  const [userEmail, setUserEmail] = useState('');
-  const [refEmail, setRefEmail] = useState('');
-  const [empId, setEmpId] = useState('');
-  const [username, setUsername] = useState('');  // This should be a text input
+  const [userEmail, setUserEmail] = useState("");
+  const [refEmail, setRefEmail] = useState("");
+  const [empId, setEmpId] = useState("");
+  const [username, setUsername] = useState(""); // This should be a text input
   const [paymentPhoto, setPaymentPhoto] = useState(null);
   const [loading, setLoading] = useState(null);
-  const [mobileNumber,setMobileNumber]=useState(null)
-  const [bankAccountNumber,setBankAccountNumber]=useState(null)
-  const [ifscCode,setIfscCode] = useState(null)
+  const [mobileNumber, setMobileNumber] = useState(null);
+  const [bankAccountNumber, setBankAccountNumber] = useState(null);
+  const [ifscCode, setIfscCode] = useState(null);
   const navigate = useNavigate();
   const dispatch = useDispatch();
-       
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading("flex");
 
     // Create FormData object
     const formData = new FormData();
-    formData.append('useremail', userEmail);
-    formData.append('refrenceemail', refEmail);
-    formData.append('mobileNumber',mobileNumber)
-    formData.append('bankAccountNumber',bankAccountNumber)
-    formData.append("empId",empId)
-    formData.append('ifscCode',ifscCode)
-    
+    formData.append("useremail", userEmail);
+    formData.append("refrenceemail", refEmail);
+    formData.append("mobileNumber", mobileNumber);
+    formData.append("bankAccountNumber", bankAccountNumber);
+    formData.append("empId", empId);
+    formData.append("ifscCode", ifscCode);
+
     if (paymentPhoto) {
-      formData.append('paymentPhoto', paymentPhoto);
-    } 
+      formData.append("paymentPhoto", paymentPhoto);
+    }
     console.log(formData);
-        
+
     try {
-      const response = await axios.post('/v1/refrence/create', formData, {
+      const response = await axios.post("/v1/refrence/create", formData, {
         headers: {
-          'Content-Type': 'multipart/form-data',
+          "Content-Type": "multipart/form-data",
         },
       });
 
-      console.log('Response:', response.data);
+      console.log("Response:", response.data);
 
       if (response.data.success) {
-        toast.success('Registration successful');
+        toast.success("Registration successful");
         setLoading(null);
         setTimeout(() => {
           dispatch(RegisterSucess());
           navigate("/");
         }, 2000);
-      } else if (response.data.message === 'Provided Email is not valid') {
+      } else if (response.data.message === "Provided Email is not valid") {
         toast.error(response.data.message);
       }
     } catch (error) {
-      console.error('Error:', error);
+      console.error("Error:", error);
       setLoading(null);
-      if (error.response && error.response.data && error.response.data.message) {
+      if (
+        error.response &&
+        error.response.data &&
+        error.response.data.message
+      ) {
         toast.error(error.response.data.message);
       } else {
-        toast.error('An error occurred. Please try again.');
+        toast.error("An error occurred. Please try again.");
       }
     }
   };
@@ -70,11 +75,25 @@ const Register = () => {
   return (
     <div className="min-h-screen flex items-center justify-center py-6 px-4 sm:px-6 lg:px-8">
       <div className="w-[90vw] xl:w-[50vw] text-black bg-white p-8 rounded-lg shadow-md">
-        <ReactLoading className={`mx-auto ${loading} sticky top-12 `} hidden type="balls" color={"#00000"} height={100} width={100} />
-        <h2 className="text-2xl font-bold text-center text-gray-900 mb-6">Register</h2>
+        <ReactLoading
+          className={`mx-auto ${loading} sticky top-12 `}
+          hidden
+          type="balls"
+          color={"#00000"}
+          height={100}
+          width={100}
+        />
+        <h2 className="text-2xl font-bold text-center text-gray-900 mb-6">
+          Register
+        </h2>
         <form onSubmit={handleSubmit} className="space-y-6">
           <div>
-            <label htmlFor="userEmail" className="block text-sm xl:text-2xl font-medium xl:font-bold text-gray-700">User Email</label>
+            <label
+              htmlFor="userEmail"
+              className="block text-sm xl:text-2xl font-medium xl:font-bold text-gray-700"
+            >
+              User Email
+            </label>
             <input
               type="email"
               id="userEmail"
@@ -87,7 +106,12 @@ const Register = () => {
           </div>
 
           <div>
-            <label htmlFor="refEmail" className="block text-sm xl:text-2xl font-medium xl:font-bold text-gray-700">Reference Email</label>
+            <label
+              htmlFor="refEmail"
+              className="block text-sm xl:text-2xl font-medium xl:font-bold text-gray-700"
+            >
+              Reference Email
+            </label>
             <input
               type="email"
               id="refEmail"
@@ -99,10 +123,13 @@ const Register = () => {
             />
           </div>
 
-
-          
           <div>
-            <label htmlFor="empId" className="block text-sm xl:text-2xl font-medium xl:font-bold text-gray-700">Employee ID</label>
+            <label
+              htmlFor="empId"
+              className="block text-sm xl:text-2xl font-medium xl:font-bold text-gray-700"
+            >
+              Employee ID
+            </label>
             <input
               type="text"
               id="empId"
@@ -110,56 +137,70 @@ const Register = () => {
               value={empId}
               onChange={(e) => setEmpId(e.target.value)}
               className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm"
-           
             />
           </div>
 
           <div>
-  <label htmlFor="mobileNumber" className="block text-sm xl:text-2xl font-medium xl:font-bold text-gray-700">Mobile Number</label>
-  <input
-    type="number"
-    id="mobileNumber"
-    name="mobileNumber"
-    value={mobileNumber}
-    onChange={(e) => setMobileNumber(e.target.value)}
-    className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm"
-    required
-  />
-</div>
-
-<div>
-  <label htmlFor="bankAccountNumber" className="block text-sm xl:text-2xl font-medium xl:font-bold text-gray-700">Bank Account Number/ UPI ID</label>
-  <input
-    type="text"
-    id="bankAccountNumber"
-    name="bankAccountNumber"
-    value={bankAccountNumber}
-    onChange={(e) => setBankAccountNumber(e.target.value)}
-    className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm"
-    required
-  />
-</div>
-
-<div>
-  <label htmlFor="ifscCode" className="block text-sm xl:text-2xl font-medium xl:font-bold text-gray-700">IFSC Code</label>
-  <input
-    type="text"
-    id="ifscCode"
-    name="ifscCode"
-    value={ifscCode}
-    onChange={(e) => setIfscCode(e.target.value)}
-    className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm"
-    required
-  />
-</div>
-
-        
-
-
-         
+            <label
+              htmlFor="mobileNumber"
+              className="block text-sm xl:text-2xl font-medium xl:font-bold text-gray-700"
+            >
+              Mobile Number
+            </label>
+            <input
+              type="number"
+              id="mobileNumber"
+              name="mobileNumber"
+              value={mobileNumber}
+              onChange={(e) => setMobileNumber(e.target.value)}
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm"
+              required
+            />
+          </div>
 
           <div>
-            <label htmlFor="paymentPhoto" className="block text-sm xl:text-2xl font-medium xl:font-bold text-gray-700">Payment Photo</label>
+            <label
+              htmlFor="bankAccountNumber"
+              className="block text-sm xl:text-2xl font-medium xl:font-bold text-gray-700"
+            >
+              Bank Account Number/ UPI ID
+            </label>
+            <input
+              type="text"
+              id="bankAccountNumber"
+              name="bankAccountNumber"
+              value={bankAccountNumber}
+              onChange={(e) => setBankAccountNumber(e.target.value)}
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm"
+              required
+            />
+          </div>
+
+          <div>
+            <label
+              htmlFor="ifscCode"
+              className="block text-sm xl:text-2xl font-medium xl:font-bold text-gray-700"
+            >
+              IFSC Code
+            </label>
+            <input
+              type="text"
+              id="ifscCode"
+              name="ifscCode"
+              value={ifscCode}
+              onChange={(e) => setIfscCode(e.target.value)}
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm"
+              required
+            />
+          </div>
+
+          <div>
+            <label
+              htmlFor="paymentPhoto"
+              className="block text-sm xl:text-2xl font-medium xl:font-bold text-gray-700"
+            >
+              Payment Photo
+            </label>
             <input
               type="file"
               id="paymentPhoto"
@@ -167,6 +208,13 @@ const Register = () => {
               onChange={(e) => setPaymentPhoto(e.target.files[0])}
               className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md"
             />
+          </div>
+
+          <div>
+            <h2>Payment</h2>
+            <a href={Payment} download="PaymentImage">
+              <img src={Payment} alt="Payment" />
+            </a>
           </div>
 
           <button
